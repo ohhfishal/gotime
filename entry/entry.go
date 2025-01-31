a2836d37206a93b61ac9792732a20714d0ba2dba
ohhfishal <ohhfishal@gmail.com>
2025-01-31 13:17:52 -0700

    dev: basic read/write

---
+++ entry/entry.go
@@ -0,0 +1,60 @@
+package entry
+
+import (
+	"bufio"
+	// "errors"
+	"fmt"
+	"io"
+	"strings"
+	"time"
+)
+
+type Entry struct {
+	Time        time.Time
+	Category    string
+	Description string
+}
+
+func ReadAll(reader io.Reader, config ...Config) ([]Entry, error) {
+	entries := []Entry{}
+	scanner := bufio.NewScanner(reader)
+	for scanner.Scan() {
+		fmt.Println("scan", scanner.Text())
+		entry, err := Read(strings.NewReader(scanner.Text()), config...)
+		if err != nil {
+			return []Entry{}, fmt.Errorf("reading line: %w", err)
+		}
+		entries = append(entries, *entry)
+	}
+
+	if err := scanner.Err(); err != nil {
+		return []Entry{}, fmt.Errorf("reading: %w", err)
+	}
+
+	return entries, nil
+}
+
+func Write(writer io.Writer, entry Entry, config ...Config) error {
+	cfg := DefaultConfig(config...)
+
+	line, err := cfg.Encode(entry)
+	if err != nil {
+		return fmt.Errorf("encoding entry: %w", err)
+	}
+
+	_, err = fmt.Fprintln(writer, line)
+	return err
+}
+
+func Read(reader io.Reader, config ...Config) (*Entry, error) {
+	cfg := DefaultConfig(config...)
+	bytes, err := io.ReadAll(reader)
+	if err != nil {
+		return nil, fmt.Errorf("reading: %w", err)
+	}
+	entry, err := cfg.Decode(string(bytes))
+	if err != nil {
+		return nil, fmt.Errorf("parsing: %w", err)
+	}
+	return entry, nil
+}
---
+++ entry/entry_test.go
@@ -0,0 +1,110 @@
+package entry
+
+import (
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	"strings"
+	"testing"
+	"time"
+)
+
+const TimeFormat = time.DateTime
+
+func TestEncodeDecode(t *testing.T) {
+	type EncodeDecodeTest struct {
+		Name  string
+		Entry Entry
+	}
+
+	tests := []EncodeDecodeTest{
+		{
+			Name:  "empty",
+			Entry: Entry{},
+		},
+		{
+			Name: "normal",
+			Entry: Entry{
+				Time:        time.Now(),
+				Category:    "Project A",
+				Description: "Important Task",
+			},
+		},
+	}
+	for _, test := range tests {
+		t.Run(test.Name, func(t *testing.T) {
+			require := require.New(t)
+
+			var buffer strings.Builder
+			err := Write(&buffer, test.Entry)
+			require.Nil(err)
+
+			t.Log(buffer.String())
+			reader := strings.NewReader(buffer.String())
+			entry, err := Read(reader)
+			require.Nil(err)
+			RequireEntryEqual(t, *entry, test.Entry)
+		})
+	}
+}
+
+func TestReadAll(t *testing.T) {
+	type Test struct {
+		Name    string
+		Entries []Entry
+	}
+	tests := []Test{
+		{
+			Name: "empty",
+		},
+		{
+			Name: "happy case",
+			Entries: []Entry{
+				{
+					Time:        time.Now(),
+					Category:    "Project A",
+					Description: "Important Task",
+				},
+				{
+					Category:    "Project B",
+					Description: "Important Task B",
+				},
+				{
+					Time:        time.Now(),
+					Category:    "Project C",
+					Description: "Important Task C",
+				},
+			},
+		},
+	}
+	for _, test := range tests {
+		t.Run(test.Name, func(t *testing.T) {
+			assert := assert.New(t)
+			require := require.New(t)
+			var buffer strings.Builder
+
+			for _, entry := range test.Entries {
+				err := Write(&buffer, entry)
+				require.Nil(err)
+			}
+
+			reader := strings.NewReader(buffer.String())
+			t.Logf("Wrote:\n%s", buffer.String())
+			entries, err := ReadAll(reader)
+			require.Nil(err)
+
+			assert.Equal(len(test.Entries), len(entries), "entries length")
+			for i, entry := range entries {
+				require.LessOrEqual(i, len(test.Entries), "out of bounds")
+				RequireEntryEqual(t, entry, test.Entries[i])
+				t.Log("Correct:", entry)
+			}
+		})
+	}
+}
+
+func RequireEntryEqual(tb testing.TB, expected, actual Entry) {
+	require := require.New(tb)
+	require.Equal(expected.Category, actual.Category)
+	require.Equal(expected.Description, actual.Description)
+	require.Equal(expected.Time.Format(TimeFormat), actual.Time.Format(TimeFormat))
+}
