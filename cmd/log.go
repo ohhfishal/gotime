97094e117de20355a40d8f6189dd5a3d22b01939
ohhfishal <ohhfishal@gmail.com>
2025-05-07 08:06:49 -0700

    feat: allow log to use arbitrary time

--- cmd/log.go
+++ cmd/log.go
@@ -8,16 +8,40 @@ import (
 )
 
 type LogCmd struct {
-	Category string        `arg:"" required:"" help:"Category to log the new entry under"`
-	Note     []string      `arg:"" optional:"" help:"Description for the entry"`
-	Offset   time.Duration `default:"0m" help:"Offset from now to set the entry as"`
+	Category string    `arg:"" required:"" help:"Category to log the new entry under"`
+	Note     []string  `arg:"" optional:"" help:"Description for the entry"`
+	Date     time.Time `short:"d" optional:"" format:"2006-01-02" help:"Date to log (default: today)"`
+	Time     time.Time `short:"t" optional:"" format:"15:04" help:"Time to log entry at (default: now)"`
 }
 
-func (cmd *LogCmd) Run(entrySet EntrySet, now func() time.Time) error {
+func (cmd *LogCmd) AfterApply() error {
+	if cmd.Date.IsZero() {
+		now := time.Now()
+		cmd.Date = time.Date(
+			now.Year(), now.Month(), now.Day(),
+			0, 0, 0, 0,
+			now.Location(),
+		)
+	}
+
+	if cmd.Time.IsZero() {
+		cmd.Time = time.Now()
+	}
+
+	// Combine time and date
+	cmd.Time = time.Date(
+		cmd.Date.Year(), cmd.Date.Month(), cmd.Date.Day(),
+		cmd.Time.Hour(), cmd.Time.Minute(), cmd.Time.Second(), 0,
+		cmd.Date.Location(),
+	)
+	return nil
+}
+
+func (cmd *LogCmd) Run(entrySet EntrySet) error {
 	newEntry := entry.Entry{
 		Category: cmd.Category,
 		Note:     strings.Join(cmd.Note, " "),
-		Time:     now().Add(cmd.Offset),
+		Time:     cmd.Time,
 	}
 	return entrySet.Append(newEntry)
 }
