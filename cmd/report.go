4b5f554531a9ebb3b478e49ae11397e76b933b65
ohhfishal <ohhfishal@gmail.com>
2025-05-08 07:16:11 -0700

    fix: report to use dates

--- cmd/report.go
+++ cmd/report.go
@@ -3,6 +3,7 @@ package cmd
 import (
 	"fmt"
 	"io"
+	"os"
 	"slices"
 	"time"
 
@@ -11,30 +12,45 @@ import (
 )
 
 type ReportCmd struct {
-	Back     time.Duration `default:"0h0m" help:"Offset to go back from 00:00 today (today@00:00 - offset)."`
-	Forward  time.Duration `default:"24h" help:"Offset to go forward from 00:00 today (today@00:00 + offset)."`
-	Template string        `optional:"" type:"existingfile" help:"text/template to use in making report"`
+	Start           time.Time `short:"s" optional:"" format:"2006-01-02" help:"Date to start report from (default: today)"`
+	End             time.Time `short:"t" optional:"" format:"2006-01-02" help:"Date to end report from (default: start + 1d)"`
+	Template        string    `type:"existingfile" help:"file text/template to use in making report"`
+	templateContent string    `kong:"-"`
 }
 
-func (cmd *ReportCmd) Run(stdout io.Writer, entrySet EntrySet, now func() time.Time) error {
-	// Resolve when to start and end
-	today, err := Today(now())
-	if err != nil {
-		return fmt.Errorf("parsing today: %w", err)
-	}
-
-	start := today.Add(-cmd.Back)
-	end := today.Add(cmd.Forward)
-
+func (cmd *ReportCmd) Run(stdout io.Writer, entrySet EntrySet) error {
 	entries, err := entrySet.GetAll()
 	if err != nil {
 		return fmt.Errorf(`reading entries: %w`, err)
 	}
 
-	filtered := entry.Filter(entries, start, end)
+	filtered := entry.Filter(entries, cmd.Start, cmd.End)
 	slices.SortFunc(filtered, entry.Compare)
-	return report.Report(report.Config{
-		Stdout:   stdout,
-		Template: cmd.Template,
-	}, filtered)
+	return report.Report(stdout,
+		cmd.templateContent,
+		filtered,
+	)
+}
+
+func (cmd *ReportCmd) AfterApply() error {
+	var err error
+	if cmd.Start.IsZero() {
+		cmd.Start, err = Today(time.Now())
+		if err != nil {
+			return fmt.Errorf(`calculating today's date: %w`, err)
+		}
+	}
+
+	if cmd.End.IsZero() {
+		cmd.End = cmd.Start.Add(24 * time.Hour)
+	}
+
+	if cmd.Template != `` {
+		bytes, err := os.ReadFile(cmd.Template)
+		if err != nil {
+			return fmt.Errorf("reading file: %w", err)
+		}
+		cmd.templateContent = string(bytes)
+	}
+	return nil
 }
