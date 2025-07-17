55c86b5ee03e0894b8676c10309d0408c3d230cd
ohhfishal <ohhfishal@gmail.com>
2025-07-17 16:01:19 -0700

    chore: move to chi for less dependencies

--- go.mod
+++ go.mod
@@ -11,7 +11,7 @@ require (
 	github.com/charmbracelet/bubbletea v1.3.5
 	github.com/charmbracelet/lipgloss v1.1.0
 	github.com/flowchartsman/swaggerui v0.0.0-20221017034628-909ed4f3701b
-	github.com/gin-gonic/gin v1.10.1
+	github.com/go-chi/chi/v5 v5.2.2
 	github.com/stretchr/testify v1.10.0
 )
 
@@ -23,45 +23,30 @@ require (
 	github.com/aymanbagabas/go-osc52/v2 v2.0.1 // indirect
 	github.com/bep/godartsass/v2 v2.5.0 // indirect
 	github.com/bep/golibsass v1.2.0 // indirect
-	github.com/bytedance/sonic v1.11.6 // indirect
-	github.com/bytedance/sonic/loader v0.1.1 // indirect
 	github.com/cenkalti/backoff/v4 v4.3.0 // indirect
 	github.com/charmbracelet/colorprofile v0.2.3-0.20250311203215-f60798e515dc // indirect
 	github.com/charmbracelet/x/ansi v0.8.0 // indirect
 	github.com/charmbracelet/x/cellbuf v0.0.13-0.20250311204145-2c3ea96c31dd // indirect
 	github.com/charmbracelet/x/term v0.2.1 // indirect
 	github.com/cli/browser v1.3.0 // indirect
-	github.com/cloudwego/base64x v0.1.4 // indirect
-	github.com/cloudwego/iasm v0.2.0 // indirect
 	github.com/creack/pty v1.1.24 // indirect
 	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/dprotaso/go-yit v0.0.0-20220510233725-9ba8df137936 // indirect
 	github.com/erikgeiser/coninput v0.0.0-20211004153227-1c3628e74d0f // indirect
 	github.com/fatih/color v1.18.0 // indirect
 	github.com/fsnotify/fsnotify v1.9.0 // indirect
-	github.com/gabriel-vasile/mimetype v1.4.3 // indirect
 	github.com/getkin/kin-openapi v0.132.0 // indirect
-	github.com/gin-contrib/sse v0.1.0 // indirect
 	github.com/go-openapi/jsonpointer v0.21.0 // indirect
 	github.com/go-openapi/swag v0.23.0 // indirect
-	github.com/go-playground/locales v0.14.1 // indirect
-	github.com/go-playground/universal-translator v0.18.1 // indirect
-	github.com/go-playground/validator/v10 v10.20.0 // indirect
 	github.com/gobwas/glob v0.2.3 // indirect
-	github.com/goccy/go-json v0.10.2 // indirect
 	github.com/gohugoio/hugo v0.147.6 // indirect
 	github.com/josharian/intern v1.0.0 // indirect
-	github.com/json-iterator/go v1.1.12 // indirect
-	github.com/klauspost/cpuid/v2 v2.2.7 // indirect
-	github.com/leodido/go-urn v1.4.0 // indirect
 	github.com/lucasb-eyer/go-colorful v1.2.0 // indirect
 	github.com/mailru/easyjson v0.7.7 // indirect
 	github.com/mattn/go-colorable v0.1.14 // indirect
 	github.com/mattn/go-isatty v0.0.20 // indirect
 	github.com/mattn/go-localereader v0.0.1 // indirect
 	github.com/mattn/go-runewidth v0.0.16 // indirect
-	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
-	github.com/modern-go/reflect2 v1.0.2 // indirect
 	github.com/mohae/deepcopy v0.0.0-20170929034955-c48cc78d4826 // indirect
 	github.com/muesli/ansi v0.0.0-20230316100256-276c6243b2f6 // indirect
 	github.com/muesli/cancelreader v0.2.2 // indirect
@@ -79,12 +64,9 @@ require (
 	github.com/spf13/afero v1.14.0 // indirect
 	github.com/spf13/cast v1.8.0 // indirect
 	github.com/tdewolff/parse/v2 v2.8.1 // indirect
-	github.com/twitchyliquid64/golang-asm v0.15.1 // indirect
 	github.com/ugorji/go/codec v1.2.12 // indirect
 	github.com/vmware-labs/yaml-jsonpath v0.3.2 // indirect
 	github.com/xo/terminfo v0.0.0-20220910002029-abceb7e1c41e // indirect
-	golang.org/x/arch v0.8.0 // indirect
-	golang.org/x/crypto v0.38.0 // indirect
 	golang.org/x/mod v0.24.0 // indirect
 	golang.org/x/net v0.40.0 // indirect
 	golang.org/x/sync v0.14.0 // indirect
--- serve/serve.go
+++ serve/serve.go
@@ -3,12 +3,15 @@ package serve
 import (
 	"context"
 	_ "embed"
+	"encoding/json"
+	"html/template"
 	"log/slog"
 	"net/http"
 	"time"
 
 	"github.com/flowchartsman/swaggerui"
-	"github.com/gin-gonic/gin"
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
 	"github.com/ohhfishal/gotime/assets"
 	"github.com/ohhfishal/gotime/entry"
 )
@@ -24,81 +27,89 @@ type EntryHandler interface {
 const errorHTML = `
 <html>
   <h1>
-    {{ .error }}
+    {{ .Error }}
   </h1>
 </html>
 `
 
+var errorTemplate = template.Must(template.New("error").Parse(errorHTML))
+
 func Serve(ctx context.Context, logger *slog.Logger, handler EntryHandler, port string) error {
-	// TODO: Have this be configurable? Or just use env? Can probably handle this in cmd/serve.go
-	// gin.SetMode(gin.DebugMode)
-	gin.SetMode(gin.ReleaseMode)
+	r := chi.NewRouter()
 
-	r := gin.New()
 	r.Use(loggingMiddleware(logger))
-	r.Use(gin.Recovery())
+	r.Use(middleware.Recoverer)
 
-	// TODO: Add this to assets and redirect
-	r.GET("/openapi.yaml", func(c *gin.Context) {
-		c.Data(http.StatusOK, "application/x-yaml", spec)
+	r.Get("/openapi.yaml", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/x-yaml")
+		w.Write(spec)
 	})
 
-	r.GET("/openapi/*filepath", gin.WrapH(http.StripPrefix("/openapi", swaggerui.Handler(spec))))
-	r.GET("/assets/*filepath", gin.WrapH(http.StripPrefix("/assets", http.FileServer(http.FS(assets.Assets)))))
-	r.GET("/favicon.ico", func(c *gin.Context) {
-		http.ServeFileFS(c.Writer, c.Request, assets.Assets, "img/favicon.ico")
+	r.Mount("/openapi", http.StripPrefix("/openapi", swaggerui.Handler(spec)))
+	r.Mount("/assets", http.StripPrefix("/assets", http.FileServer(http.FS(assets.Assets))))
+
+	r.Get("/favicon.ico", func(w http.ResponseWriter, r *http.Request) {
+		http.ServeFileFS(w, r, assets.Assets, "img/favicon.ico")
 	})
 
-	r.GET("/", func(c *gin.Context) {
+	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
 		// TODO: Have this be handled via HTMX and GET endpoint
 		entries, err := handler.GetAllEntries(entry.Today())
 		if err != nil {
 			// TODO: Make a template for this
-			c.HTML(http.StatusInternalServerError, errorHTML, gin.H{"error": err.Error()})
+			w.WriteHeader(http.StatusInternalServerError)
+			errorTemplate.Execute(w, map[string]string{"Error": err.Error()})
 			return
 		}
 		MainPage(
 			entry.Summarize(entries),
-		).Render(c, c.Writer)
+		).Render(r.Context(), w)
 	})
 
-	api := r.Group("/api/v1")
-	{
-		api.GET("/entry", func(c *gin.Context) {
+	r.Route("/api/v1", func(r chi.Router) {
+		r.Get("/entry", func(w http.ResponseWriter, r *http.Request) {
 			entries, err := handler.GetAllEntries(entry.Today())
 			if err != nil {
-				c.HTML(http.StatusInternalServerError, errorHTML, gin.H{"error": err.Error()})
+				w.WriteHeader(http.StatusInternalServerError)
+				errorTemplate.Execute(w, map[string]string{"Error": err.Error()})
 				return
 			}
 			Details(
 				entry.Summarize(entries),
-			).Render(c, c.Writer)
+			).Render(r.Context(), w)
 
 			// TODO: Filter entries based on some parameters
 			// TODO: Have this return HTML or JSON based on the header
-			// c.JSON(http.StatusOK, entries)
+			// w.Header().Set("Content-Type", "application/json")
+			// json.NewEncoder(w).Encode(entries)
 		})
 
-		api.POST("/entry", func(c *gin.Context) {
+		r.Post("/entry", func(w http.ResponseWriter, r *http.Request) {
 			var entryData entry.Entry
-			if err := c.ShouldBindJSON(&entryData); err != nil {
-				c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			if err := json.NewDecoder(r.Body).Decode(&entryData); err != nil {
+				w.Header().Set("Content-Type", "application/json")
+				w.WriteHeader(http.StatusBadRequest)
+				json.NewEncoder(w).Encode(map[string]string{"error": err.Error()})
 				return
 			}
 
 			// TODO: Actually validate the entry!
 
 			if err := handler.CreateEntry(entryData); err != nil {
-				c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+				w.Header().Set("Content-Type", "application/json")
+				w.WriteHeader(http.StatusInternalServerError)
+				json.NewEncoder(w).Encode(map[string]string{"error": err.Error()})
 				return
 			}
 			// TODO: Have this return HTML or JSON or text/plain based on the header
-			c.Status(http.StatusOK)
+			w.WriteHeader(http.StatusOK)
 		})
-	}
+	})
 
-	r.NoRoute(func(c *gin.Context) {
-		c.JSON(http.StatusNotFound, gin.H{"error": "Not Found"})
+	r.NotFound(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusNotFound)
+		json.NewEncoder(w).Encode(map[string]string{"error": "Not Found"})
 	})
 
 	server := &http.Server{
@@ -125,15 +136,30 @@ func Serve(ctx context.Context, logger *slog.Logger, handler EntryHandler, port 
 	return nil
 }
 
-func loggingMiddleware(logger *slog.Logger) gin.HandlerFunc {
-	return func(c *gin.Context) {
-		start := time.Now()
-		c.Next()
-		logger.Info("replied to request",
-			"method", c.Request.Method,
-			"path", c.Request.URL.Path,
-			"status", c.Writer.Status(),
-			"duration", time.Since(start).String(),
-		)
+func loggingMiddleware(logger *slog.Logger) func(http.Handler) http.Handler {
+	return func(next http.Handler) http.Handler {
+		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			start := time.Now()
+
+			ww := &responseWriter{ResponseWriter: w, statusCode: http.StatusOK}
+			next.ServeHTTP(ww, r)
+
+			logger.Info("replied to request",
+				"method", r.Method,
+				"path", r.URL.Path,
+				"status", ww.statusCode,
+				"duration", time.Since(start).String(),
+			)
+		})
 	}
+}
+
+type responseWriter struct {
+	http.ResponseWriter
+	statusCode int
+}
+
+func (rw *responseWriter) WriteHeader(code int) {
+	rw.statusCode = code
+	rw.ResponseWriter.WriteHeader(code)
 }
